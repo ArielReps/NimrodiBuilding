4b8479b301b98c5bf796b6bfd7dfde2bd4b2129d
arielrepos <ariel_ohana@webiks.com>
2024-11-05 14:46:48 +0200

    Changed for deploy

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import floorAccessReducer from './floorreducer'; // Import the items reducer
-import rolesReducer from './rolereducer'
+import floorAccessReducer from './Floorreducer'; // Import the items reducer
+import rolesReducer from './Rolereducer'
 const store = configureStore({
   reducer: {
     floorAccess: floorAccessReducer, // Combining reducers if necessary
