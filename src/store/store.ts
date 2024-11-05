f7e02066538f26858d5f0225e0b582c0a9b93ac8
arielrepos <ariel_ohana@webiks.com>
2024-11-05 14:55:02 +0200

    Stam change

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import floorAccessReducer from './Floorreducer'; // Import the items reducer
-import rolesReducer from './Rolereducer'
+import floorAccessReducer from './blalafloorreducer'; // Import the items reducer
+import rolesReducer from './blablarolereducer'
 const store = configureStore({
   reducer: {
     floorAccess: floorAccessReducer, // Combining reducers if necessary
